6393139289e65bd738c266d1868955808e82cebd
hojixv <153679337+hojixv@users.noreply.github.com>
2024-09-27 05:51:10 -0400

    Web.py

---
+++ Web.py
@@ -0,0 +1,43 @@
+# Web
+
+import Web
+import aiohttp
+import random
+import asyncio
+
+invalid_Web = 0
+
+# set blacklisted ips
+blacklisted_ips = []
+
+# Json
+import json
+
+# As u can see it uses 'Athhp' *_*
+import subprocess
+intents.typing = True # intent detection #1
+import random
+import threading
+# update title
+import os # Os
+
+# Varibles 
+
+found = False
+x = 0
+mt = 0
+
+web_index = open("T.txt").read().splitlines()
+
+def clear_log_file():
+  file_Path = "T.txt"
+try:
+
+# Set intents
+
+intents.typing = False
+intents.presences = False
+
+Delay []
+WebUrl = ""
+Timeout:
